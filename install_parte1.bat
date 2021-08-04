2a60df81a9694f5ddc7c6bf510950b3ab99b216d
Paulo R. P. Santiago <35745703+paulopreto@users.noreply.github.com>
2021-08-04 14:09:16 -0700

    Update install_parte1.bat

--- install_parte1.bat
+++ install_parte1.bat
@@ -1,11 +1,8 @@
 @echo off
-md C:\PATH_programs
-md C:\PATH_programs\FFmpeg
 md C:\PATH_programs\FFmpeg\bin
 md C:\PATH_programs\FFmpeg\doc
 md C:\PATH_programs\FFmpeg\presets
 
-copy PATH_programs\FFmpeg\* C:\PATH_programs\FFmpeg
 copy PATH_programs\FFmpeg\bin\* C:\PATH_programs\FFmpeg\bin
 copy PATH_programs\FFmpeg\doc\* C:\PATH_programs\FFmpeg\doc
 copy PATH_programs\FFmpeg\presets\* C:\PATH_programs\FFmpeg\presets
